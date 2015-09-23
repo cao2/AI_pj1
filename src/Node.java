5e429411d4e059b853b7b7fc20eef3bb27322ad2
cao2 <cao2@mail.usf.edu>
2015-09-22 23:53:58 -0400

    kk

--- src/Node.java
+++ src/Node.java
@@ -4,21 +4,39 @@ public class Node {
 	
     private String name; 
     private Map<Node,Integer> child=new HashMap<Node,Integer>(); 
+    private int[] cord;
     
+    //constructor
     public Node(String name){ 
-        this.name=name; 
+        this.name=name;
+        cord=new int[2];
     } 
+    //coordinate
+    public void setCord(int x, int y){
+    	cord[0]=x;
+    	cord[1]=y;
+    }
+    public int[] getCord(){
+    	return cord;
+    }
+   //name 
    public String getName() { 
         return name; 
     } 
     public void setName(String name) { 
         this.name = name; 
     } 
+    //connection
     public Map<Node, Integer> getChild() { 
         return child; 
     } 
-    
     public void setChild(Map<Node,Integer> child) { 
         this.child = child; 
     } 
+    public String toString(){
+    	String conn="";
+    	for(Node tmp:child.keySet())
+    		conn+=" "+tmp.getName();
+    	return name+" "+cord[0]+","+cord[1]+"conn:"+conn;
+    }
 }
--- src/drive.java
+++ src/drive.java
@@ -1,4 +1,91 @@
+import java.io.*;
+import java.util.*;
 
 public class drive {
+//main
+	public static void main(String[] args) throws IOException{
 
+		ArrayList<Node> allNode=new ArrayList<Node> ();
+		ArrayList<String> allName=new ArrayList<String> ();
+		
+		
+		
+		//read file: locsamp
+		BufferedReader br = new BufferedReader(new FileReader("locsamp.txt"));
+		try {
+		    String line = br.readLine();
+		    while (line != null) {
+		        //check if end of the file
+		        if (line.equals("END"))
+		        	break;
+		        
+		        String[] tmp= line.split("\\s");
+		        Node pt=new Node(tmp[0]);
+		        pt.setCord(Integer.parseInt(tmp[1]), Integer.parseInt(tmp[2]));
+		        allNode.add(pt);
+		        allName.add(tmp[0]);
+		        line = br.readLine();
+
+		    }
+		} finally {
+		    br.close();
+		}
+		/**
+		for(String tmp:allName)
+			System.out.println(tmp);
+		System.out.println("***************");
+		for(Node tmp:allNode)
+			System.out.println(tmp.getName());
+			**/
+		//read file: connsamp
+		 br = new BufferedReader(new FileReader("connsamp.txt"));
+		try {
+		    String line = br.readLine();
+		    while (line != null) {
+		        line = br.readLine();
+		        //check if end of the file
+		        if (line.equals("END"))
+		        	break;
+		        String[] tmp= line.split("\\s");
+		         Map<Node,Integer> child=new HashMap<Node,Integer>(); 
+		         //System.out.println(tmp[0]+" **********************");
+		         for(int i=0;i<Integer.parseInt(tmp[1]);i++){
+		        	// System.out.println(tmp[2+i]);
+		        	 int ind=allName.indexOf(tmp[2+i]);
+		        	 //System.out.println(ind);
+		        	 child.put(allNode.get(ind), 0);
+		         }
+		        String name=tmp[0];
+		        int ind=allName.indexOf(name);
+		        allNode.get(ind).setChild(child);
+		    }
+		} finally {
+		    br.close();
+		}
+		System.out.println("finished processing file, result:");
+		for(Node x:allNode)
+			System.out.println(x.toString());
+		
+		//ask for initial point and target point
+				Scanner inp=new Scanner(System.in);
+				while(true){
+					System.out.print("initial point:");
+					String stat=inp.nextLine();
+					if(allName.contains(stat))
+						break;
+					System.out.println("point not exist");
+					
+				}
+				while(true){
+					System.out.print("target point:");
+					String tgt=inp.nextLine();
+					if(allName.contains(tgt))
+						break;
+					System.out.println("point not exist");
+					
+				}
+				
+		
+	}
+	//end of main
 }
