f2cd25032937606f3acfa5a63641b3dc421c18c9
cao2 <cao2@mail.usf.edu>
2015-10-21 05:31:04 -0400

    adsfads

---
+++ src/Graph.java
@@ -0,0 +1,15 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Graph extends JPanel {
+	ArrayList<Node> allNode;
+
+	public Graph(ArrayList<Node> x){
+		super();
+		allNode=x;
+	}
+	
+
+}
--- src/PointPanel.java
+++ src/PointPanel.java
@@ -1,16 +1,17 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.GridLayout;
-import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import javax.swing.*;
+
 
 public class PointPanel extends JComponent{
 	ArrayList<Node> allNode=new ArrayList<Node> ();
@@ -34,7 +35,7 @@ public class PointPanel extends JComponent{
 	    JPanel loc=new JPanel();
 	    JTextArea location=new JTextArea("coordinate: ");
 	    JTextField locationf=new JTextField(20);
-	    locationf.setText("/Users/cao2/Documents/workspace/AI_pj1/locsamp.txt");
+	    locationf.setText("/Users/yutingcao/Documents/AI_pj1/locsamp.txt");
 	    JButton locationb=new JButton("..");
 	    loc.add(location);
 	    loc.add(locationf);
@@ -43,7 +44,7 @@ public class PointPanel extends JComponent{
 	    JPanel cor=new JPanel();
 	    JTextArea cord=new JTextArea("connection: ");
 	    JTextField corf=new JTextField(20);
-	    corf.setText("/Users/cao2/Documents/workspace/AI_pj1/connsamp.txt");
+	    corf.setText("/Users/yutingcao/Documents/AI_pj1/connsamp.txt");
 	    JButton corb=new JButton("..");
 	    cor.add(cord);
 	    cor.add(corf);
@@ -128,12 +129,12 @@ public class PointPanel extends JComponent{
 	    		    }
 	    		    br.close();
 	    		    System.out.println("finished reading each file");
+	    		    yuFrame.setLayout(new BoxLayout(loadPanel, BoxLayout.LINE_AXIS));
 	    		    yuFrame.remove(loadPanel);
-	    		    yuFrame.setSize(1500, 1200);
+	    		    yuFrame.setSize(1200, 1000);
 	    		    yuFrame.setContentPane(drawpoint());
 	    		    yuFrame.validate();
 	    		    yuFrame.repaint();
-	    		    
 	    		    
 	    		} catch(IOException ex) {
 	    		   System.out.println(ex.getMessage());
@@ -150,12 +151,13 @@ public class PointPanel extends JComponent{
 
 	public JPanel drawpoint(){
 		JPanel rst=new JPanel();
-		
+		rst.setLayout(new BoxLayout(rst,BoxLayout.LINE_AXIS));
 		JPanel left=new JPanel();
 	    left.setLayout(new BoxLayout(left, BoxLayout.PAGE_AXIS));
+	    //left.setPreferredSize(new Dimension());
 	    //initial input
 		JPanel init=new JPanel();
-	    JTextArea ini=new JTextArea("start point: ");
+	    JLabel ini=new JLabel("start point: ");
 	    String[] allNamelist=new String[allName.size()];
 	    allNamelist=allName.toArray(allNamelist);
 	    JComboBox<String> initc=new JComboBox<String>(allNamelist);
@@ -163,14 +165,14 @@ public class PointPanel extends JComponent{
 	    init.add(initc);
 	    //target input
 	    JPanel tgt=new JPanel();
-	    JTextArea tgtt=new JTextArea("start point: ");
+	    JLabel tgtt=new JLabel("target point: ");
 	    JComboBox<String> tgtc=new JComboBox<String>(allNamelist);
 	    tgt.add(tgtt);
 	    tgt.add(tgtc);
 		
-	    JTextArea disable=new JTextArea("mark points you want to disable: ");
+	    JLabel disable=new JLabel("mark points you want to disable: ");
 	    JPanel dis=new JPanel();
-	    GridLayout grid = new GridLayout(0,5);
+	    GridLayout grid = new GridLayout(0,3);
 	    dis.setLayout(grid);
 	    JCheckBox[] checks=new JCheckBox[allNamelist.length];
 	    for(int i=0;i<allNamelist.length;i++){
@@ -184,12 +186,55 @@ public class PointPanel extends JComponent{
 		left.add(disable);
 		left.add(dis);
 		left.add(start);
+		left.add(Box.createRigidArea(new Dimension(0,500)));
+
 		
-		JFrame right=new JFrame();
-		
-		
+		left.setPreferredSize(new Dimension(250,700));
 		rst.add(left);
+		points right=new points();
 	    rst.add(right);
 		return rst;
 	}
+	public class points extends JPanel {
+
+        private static final long serialVersionUID = 1L;
+        points() {
+            setLayout(new BorderLayout());   
+        }
+        
+        
+        
+        
+        @Override
+        public void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+            AffineTransform forma = AffineTransform.getTranslateInstance(300,700);
+            forma.scale(1.1, 1.1);
+            g2d.setTransform(forma);
+            g2d.setPaint(Color.blue);
+        	int r=21;
+            for(Node x:allNode){
+            	int x1=x.getCord()[0];
+            	int y1=x.getCord()[1];
+            	int m=x1-r/2;
+            	int n=-y1-r/2;
+            	g2d.setColor(Color.blue);
+            	g2d.fillOval(m,n,r,r);
+                g2d.setColor(Color.blue);
+            	for(Node y:x.getChild()){
+            		int x2[]=y.getCord();
+            		g2d.drawLine(x1, -y1, x2[0], -x2[1]);
+            	}
+            	g2d.setColor(Color.black);
+            	g2d.drawString(x.getName(), x1-10, -y1+2);
+
+            }
+        	
+            super.paintComponent(g);
+ 
+        }
+    }
+	
 }
